216680d78a6d4fd8a09ce9da14b4aeeee0ef5f97
AlzPatz <alexpatersonsmail@gmail.com>
2020-08-13 11:59:54 +0100

    changing folder structure

---
+++ src/ci_experiment/function.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ci_experiment
+{
+    public class Function
+    {
+		public static void Speak()
+		{
+			Console.WriteLine(" - boring Should ACTUALLY NOW !!! only show on MYGET feed");
+		}
+
+		public static void SomethingElse()
+		{
+			Console.WriteLine("This does something else #5 I guess");
+		}
+    }
+}
