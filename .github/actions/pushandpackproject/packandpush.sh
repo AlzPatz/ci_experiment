439e470e14f178b85bdb0bc81dc8e5952aa0f065
AlzPatz <alexpatersonsmail@gmail.com>
2020-08-20 17:27:53 +0100

    Update packandpush.sh

--- .github/actions/pushandpackproject/packandpush.sh
+++ .github/actions/pushandpackproject/packandpush.sh
@@ -51,33 +51,13 @@ for dir in *; do
         # Will not run if directory name has test within it
         if [[ ${dir} != *"test"* ]] && [[ ${dir} != *"Test"* ]]; then
             cd $dir
-            echo "PACK"
-            ls
+            
+            dotnet pack -c $BUILD_CONFIGURATION -o .
+            dotnet nuget push *.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY --skip-duplicate 
+            
+            mv cp *.nupkg ${ROOT}/packages/
+            
+            cd ..
         fi
     fi
 done
-
-#echo "Environment Variables:"
-#echo $TOP_LEVEL_SOURCE_DIR
-#echo $PACKAGE_NAME_SUFFIX
-#echo $USE_NAME_SUFFIX
-#echo $VERSION_STRING
-#echo $NUGET_SOURCE
-#echo $NUGET_API_KEY
-#echo "-----"
-#echo "Current directory"
-#pwd
-#echo $PWD
-#ls
-#ROOT=$PWD
-#echo "Move to top level source"
-#cd $TOP_LEVEL_SOURCE_DIR
-#pwd
-#ls
-#cd $ROOT
-#echo "what's here"
-#ls
-#cd packages
-#pwd
-#touch ${VERSION_STRING}.txt
-#ls
